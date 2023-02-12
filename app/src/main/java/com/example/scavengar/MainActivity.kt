7b2d129dd95228e3d075d40cfad69c7be3d8b035
Areeb-510 <areeb510@gmail.com>
2023-02-12 08:45:24 +0530

    Chnages made

--- app/src/main/java/com/example/scavengar/MainActivity.kt
+++ app/src/main/java/com/example/scavengar/MainActivity.kt
@@ -43,7 +43,6 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var arscan : Button
 
-//    private lateinit var scanner : Button
 
     private var oat : Location = Location("")
     private var dbh : Location = Location("")
@@ -53,6 +52,9 @@ class MainActivity : AppCompatActivity() {
     private var myLocation : Location = Location("")
 
     private val modelNode = ModelNode()
+
+
+    private lateinit var clickTest : Button
 
 //    private lateinit var sceneView: ArSceneView
 
@@ -66,6 +68,7 @@ class MainActivity : AppCompatActivity() {
 
         fetchLocation = findViewById(R.id.fetchlocation)
         arscan = findViewById(R.id.arscan)
+        clickTest = findViewById(R.id.testbtn)
 
         oat.latitude = 31.7060450
         oat.longitude = 76.5254121
@@ -82,59 +85,50 @@ class MainActivity : AppCompatActivity() {
         juicebar.latitude = 31.7054263
         juicebar.longitude = 76.5276316
 
-//        scanner = findViewById(R.id.scanner)
 
-//        sceneView = findViewById(R.id.sceneView)
-//
-//        sceneView.addChild(modelNode)
+        clickTest.setOnClickListener {
+            Toast.makeText(this,"Clicked",Toast.LENGTH_LONG).show()
+            val intent = Intent(this,ScanActivity::class.java)
+            intent.putExtra("glbname","nokia")
+            startActivity(intent)
+        }
 
 
         fetchLocation.setOnClickListener {
-//            Toast.makeText(this,"No clue in the location",Toast.LENGTH_LONG).show()
             getLocation()
-
         }
 
         arscan.setOnClickListener {
 
             if(myLocation.checkIsInBound(20.0,oat)){
                 Toast.makeText(this,"OAT",Toast.LENGTH_LONG).show()
-//                scanner.visibility = View.VISIBLE
                 val intent = Intent(this,ScanActivity::class.java)
                 intent.putExtra("glbname","racket")
                 startActivity(intent)
             }else if(myLocation.checkIsInBound(20.0,tennis)){
-                Toast.makeText(this,"Tennis Court",Toast.LENGTH_LONG).show()
+                Toast.makeText(this,"TENNIS COURT",Toast.LENGTH_LONG).show()
                 val intent = Intent(this,ScanActivity::class.java)
-                intent.putExtra("glbname","phone")
+                intent.putExtra("glbname","nokia")
                 startActivity(intent)
-//                scanner.visibility = View.VISIBLE
             }else if(myLocation.checkIsInBound(20.0,juicebar)){
                 Toast.makeText(this,"JUICE BAR",Toast.LENGTH_LONG).show()
                 val intent = Intent(this,ScanActivity::class.java)
-//                scanner.visibility = View.VISIBLE
-                intent.putExtra("glbname","Spoons")
+                intent.putExtra("glbname","mapsnew")
+                startActivity(intent)
+            }else if(myLocation.checkIsInBound(20.0,dbh)){
+                Toast.makeText(this,"DBH",Toast.LENGTH_LONG).show()
+                val intent = Intent(this,ScanActivity::class.java)
+                intent.putExtra("glbname","dumbbell")
                 startActivity(intent)
             }else if(myLocation.checkIsInBound(20.0,opengym)){
                 Toast.makeText(this,"OPEN GYM",Toast.LENGTH_LONG).show()
                 val intent = Intent(this,ScanActivity::class.java)
-//                scanner.visibility = View.VISIBLE
-                intent.putExtra("glbname","racket")
+                intent.putExtra("glbname","congo")
                 startActivity(intent)
-            }else if(myLocation.checkIsInBound(20.0,opengym)){
-                Toast.makeText(this," Co",Toast.LENGTH_LONG).show()
-                val intent = Intent(this,ScanActivity::class.java)
-                intent.putExtra("glbname","phone")
-                startActivity(intent)
-//                scanner.visibility = View.VISIBLE
-            }
-            else{
+            }else{
                 Toast.makeText(this,"Please scan first",Toast.LENGTH_LONG).show()
             }
 
-//            lifecycleScope.launch {
-//                ARView()
-//            }
         }
 
 
@@ -235,20 +229,6 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-//
-//    suspend fun ARView(){
-//
-//        Toast.makeText(this,"Started",Toast.LENGTH_LONG).show()
-//        modelNode.loadModelGlb(
-//            context = this,
-//            glbFileLocation = "models/Spoons.glb",
-//            lifecycle = lifecycle,
-//            autoAnimate = true,
-//            centerOrigin = Position(x = -1.0f, y = 1.0f,z = 0.0f),
-//            onError = { exception -> }
-//        )
-//
-//    }
 
 
 
